3b706ed71e57ccaed81cfd1de26eb461493e83a5
Andrew Roan <roanutil@users.noreply.github.com>
2024-09-19 09:23:47 -0500

    Bump minimum iOS and Swift verions
    feature/v20.1.0

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
     name: "intune-ios-sdk",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v16),
     ],
     products: [
         .library(name: "IntuneMAMSwift", targets: ["IntuneMAMSwift"]),
